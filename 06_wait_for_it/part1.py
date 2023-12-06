b9fbf79899dd26e45207ecd9cd914c564539284e
Connor Lane Smith <cls@lubutu.com>
2023-12-06 08:10:05 +0000

    Day 6: I hadn't expected brute force to work

--- 06_wait_for_it/part1.py
+++ 06_wait_for_it/part1.py
@@ -14,16 +14,19 @@ def parse_races(lines):
             races[index][key] = value
     return races
 
-def ways_to_win(race):
-    time = race['Time']
-    distance = race['Distance']
-    for hold_time in range(time):
-        move_time = time - hold_time
-        if hold_time * move_time > distance:
-            yield hold_time
+def solve_quadratic(a, b, c):
+    d = math.sqrt((b ** 2) - (4 * a * c))
+    root1 = (-b - d) / (2 * a)
+    root2 = (-b + d) / (2 * a)
+    return root1, root2
 
 def number_of_ways_to_win(race):
-    return sum(1 for hold_time in ways_to_win(race))
+    time = race['Time']
+    distance = race['Distance']
+    min_root, max_root = solve_quadratic(1, -time, distance + 1)
+    min_hold = math.ceil(min_root)
+    max_hold = math.floor(max_root)
+    return max_hold - min_hold + 1
 
 if __name__ == '__main__':
     races = parse_races(sys.stdin)
